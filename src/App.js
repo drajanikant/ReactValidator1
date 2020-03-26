c3074a924987a261f8c901607b9e77c3c263c7a9
Rajanikant Devmore <rajanikantdevmore@outlook.com>
2020-03-27 01:01:50 +0530

    Updated the revalidator package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from "react";
 import "./styles.css";
+import revalidator from "revalidator";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { user: "" };
+  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submit = e => {
+    console.log(
+      revalidator.validate(this.state, {
+        properties: {
+          url: {
+            description: "the url the object should be stored at",
+            type: "string",
+            pattern: "^/[^#%&*{}\\:<>?/+]+$",
+            required: true
+          },
+          user: {
+            description:
+              "a means of protecting data (insufficient for production, used as example)",
+            type: "string",
+            minLength: 5
+          },
+          body: {
+            description: "what to store at the url",
+            type: "any",
+            default: null
+          }
+        }
+      })
+    );
+  };
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <input type="text" name="user" value={user} onChange={this.onChange} />
+
+        <button type="button" onClick={this.submit}>
+          Check
+        </button>
+      </div>
+    );
+  }
 }
