1c73dc88e91665b66f2e82eccf66d8a26cdb9c5f
Rajanikant Devmore <rajanikantdevmore@outlook.com>
2020-03-27 01:27:08 +0530

    Updated with error messages

--- src/App.js
+++ src/App.js
@@ -6,44 +6,55 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { user: "" };
+    this.state = { user: "", email: "" };
   }
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   submit = e => {
-    console.log(
-      revalidator.validate(this.state, {
-        properties: {
-          url: {
-            description: "the url the object should be stored at",
-            type: "string",
-            pattern: "^/[^#%&*{}\\:<>?/+]+$",
-            required: true
-          },
-          user: {
-            description:
-              "a means of protecting data (insufficient for production, used as example)",
-            type: "string",
-            minLength: 5
-          },
-          body: {
-            description: "what to store at the url",
-            type: "any",
-            default: null
-          }
+    const valid = revalidator.validate(this.state, {
+      properties: {
+        user: {
+          description:
+            "a means of protecting data (insufficient for production, used as example)",
+          type: "string",
+          minLength: 5,
+          allowEmpty: false
+        },
+        email: {
+          description:
+            "a means of protecting data (insufficient for production, used as example)",
+          type: "string",
+          pattern: "^/[^#%&*{}\\:<>?/+]+$",
+          allowEmpty: false
         }
-      })
-    );
+      }
+    });
+    console.log(valid);
+    const messages = valid.errors.reduce((errors, error) => {
+      if (errors[error.property]) {
+        errors[error.property].push(error.message);
+      } else {
+        errors[error.property] = [error.message];
+      }
+      return errors;
+    }, {});
+
+    console.log(messages);
   };
   render() {
-    const { user } = this.state;
+    const { user, email } = this.state;
 
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <input type="text" name="user" value={user} onChange={this.onChange} />
-
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={this.onChange}
+        />
         <button type="button" onClick={this.submit}>
           Check
         </button>
